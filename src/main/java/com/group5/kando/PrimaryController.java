cfa5b3b7e44d00feb4fc894072c1f4f770bc49e2
chrisMasoud <chrismasoud22@gmail.com>
2023-03-07 12:18:43 -0500

    Updated dependencies

--- src/main/java/com/group5/kando/PrimaryController.java
+++ src/main/java/com/group5/kando/PrimaryController.java
@@ -1,9 +1,19 @@
 package com.group5.kando;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
+
+    FileInputStream serviceAccount
+            = new FileInputStream("/Users/cgmasoud/NetBeansProjects/KanDo/kando-project-management-firebase-adminsdk-nkidp-256da28ef7.json");
+
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+    FirebaseApp.initializeApp (options);
 
     @FXML
     private void switchToSecondary() throws IOException {
