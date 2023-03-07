fa7e651181a154b4dc6e4c9b372b928672443982
chrisMasoud <chrismasoud22@gmail.com>
2023-03-07 12:55:18 -0500

    Initialized Firestore database

--- src/main/java/com/group5/kando/PrimaryController.java
+++ src/main/java/com/group5/kando/PrimaryController.java
@@ -1,20 +1,36 @@
 package com.group5.kando;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    FileInputStream serviceAccount
-            = new FileInputStream("/Users/cgmasoud/NetBeansProjects/KanDo/kando-project-management-firebase-adminsdk-nkidp-256da28ef7.json");
+    public void initialize() {
+        try {
+            InputStream serviceAccount = new FileInputStream("/Users/cgmasoud/NetBeansProjects/KanDo/kando-project-management-firebase-adminsdk-nkidp-256da28ef7.json");
+    
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .build();
 
-    FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
+            FirebaseApp.initializeApp(options);
 
-    FirebaseApp.initializeApp (options);
-
+            Firestore db = FirestoreClient.getFirestore();
+        } catch (IOException e) {
+            System.out.println("Could not connect to Firestore.");
+            e.printStackTrace();
+        }
+        
+    }
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
