eca001f2e9f9ccd7493aa1887db292b7edc4e53e
chrisMasoud <chrismasoud22@gmail.com>
2023-03-08 15:47:53 -0500

    .gitignore is now working

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.group5.kando {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.api.apicommon;
+    requires com.google.auth.oauth2;
+    requires firebase.admin;
+    requires google.cloud.firestore;
 
     opens com.group5.kando to javafx.fxml;
     exports com.group5.kando;
