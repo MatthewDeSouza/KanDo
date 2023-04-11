a4070b7f965e830f1a9cc501533065dec18383cb
chrisMasoud <chrismasoud22@gmail.com>
2023-04-11 15:29:05 -0400

    Renamed package

---
+++ src/main/java/com/groupfive/kando/backend/exception/UserHasTicketException.java
@@ -0,0 +1,7 @@
+package com.groupfive.kando.backend.exception;
+
+public class UserHasTicketException extends Exception {
+    public UserHasTicketException(String message) {
+        super(message);
+    }
+}
