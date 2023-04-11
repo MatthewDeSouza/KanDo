07b11532146f68bdb24f75599c747a7a6275bdaf
chrisMasoud <chrismasoud22@gmail.com>
2023-04-11 15:30:27 -0400

    Updated module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,16 @@
-module com.group5.kando {
+module com.groupfive.kando {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.google.api.apicommon;
+    //requires com.google.api.apicommon;
+    requires com.google.auth;
     requires com.google.auth.oauth2;
     requires firebase.admin;
     requires google.cloud.firestore;
 
-    opens com.group5.kando to javafx.fxml;
-    exports com.group5.kando;
-    exports com.group5.kando.backend.exception;
-    exports com.group5.kando.backend;
-    exports com.group5.kando.backend.status;
-    opens com.group5.kando.backend to javafx.fxml;
+    opens com.groupfive.kando to javafx.fxml;
+    exports com.groupfive.kando;
+    exports com.groupfive.kando.backend.exception;
+    exports com.groupfive.kando.backend.classes;
+    exports com.groupfive.kando.backend.status;
+    opens com.groupfive.kando.backend.classes to javafx.fxml;
 }
