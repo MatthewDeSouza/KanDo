015eba029a0885172fe6f10b66f867900f99071f
chrisMasoud <chrismasoud22@gmail.com>
2023-04-13 13:09:25 -0400

    Updated package name

---
+++ src/main/java/com/groupfive/kando/backend/classes/Ticket.java
@@ -0,0 +1,84 @@
+package com.groupfive.kando.backend.classes;
+
+import com.groupfive.kando.backend.exception.TicketAlreadyCompleteException;
+import com.groupfive.kando.backend.status.Status;
+
+public class Ticket {
+    private String title;
+    private String content;
+    private Status status;
+
+    /**
+     * Constructor to create a Ticket, and assign it to the {@link Status} backlog.
+     *
+     * @param title   Title of ticket.
+     * @param content Content of ticket.
+     */
+    public Ticket(String title, String content) {
+        this.title = title;
+        this.content = content;
+        status = Status.BACKLOG;
+    }
+
+    /**
+     * Constructor to assign all variables contained within the class.
+     *
+     * @param title Title of the ticket.
+     * @param content Content of the ticket.
+     * @param status Status of the ticket.
+     * @author Matthew DeSouza
+     */
+    public Ticket(String title, String content, Status status) {
+        this.title = title;
+        this.content = content;
+        this.status = status;
+    }
+
+    /**
+     * Increments the ticket according to the current {@link Status} assigned to it.
+     *
+     * @throws TicketAlreadyCompleteException Thrown when the Ticket is already of {@link Status} DONE.
+     * @author Matthew DeSouza
+     */
+    public void incrementTicket() throws TicketAlreadyCompleteException {
+        switch (status) {
+            case BACKLOG:
+                status = Status.TODO;
+                break;
+            case TODO:
+                status = Status.WORKING;
+                break;
+            case WORKING:
+                status = Status.DONE;
+                break;
+            case DONE:
+                throw new TicketAlreadyCompleteException("Ticket is already done! [status=" + status.name() + "].");
+        }
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+}
