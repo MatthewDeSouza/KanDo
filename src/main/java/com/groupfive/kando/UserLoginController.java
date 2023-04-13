dc570e6e57e11e0504137072df25ec0a274dbcb1
chrisMasoud <chrismasoud22@gmail.com>
2023-04-13 13:09:54 -0400

    Created Login Functionality

--- src/main/java/com/groupfive/kando/UserLoginController.java
+++ src/main/java/com/groupfive/kando/UserLoginController.java
@@ -1,10 +1,14 @@
 package com.groupfive.kando;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.groupfive.kando.backend.classes.User;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class UserLoginController {
@@ -13,16 +17,24 @@ public class UserLoginController {
     private TextField textFieldEmail;
     @FXML
     private TextField textFieldPassword;
+    @FXML
+    private Label labelLoginFailed;
+    private Firestore db;
+    private User admin = new User("admin@kando.com");
+    private User manager = new User("manager@kando.com");
+    private User teamMember = new User("teammember@kando.com");
 
     public void initialize() {
-
+        admin.setPassword("abcdef");
+        manager.setPassword("abcdef");
+        teamMember.setPassword("abcdef");
         try {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("key.json")))
                     .build();
             FirebaseApp.initializeApp(options);
-
-        } catch (IOException e) {
+            db = FirestoreClient.getFirestore();
+        } catch (Exception e) {
             System.out.println("Could not connect to Firestore.");
             e.printStackTrace();
         }
@@ -30,11 +42,19 @@ public class UserLoginController {
 
     public void handleLogin() {
         try {
-            //auth = FirebaseAuth.getInstance();
-            //String email = textFieldEmail.getText();
-            //String password = textFieldPassword.getText();
-            //auth.signInWithEmailAndPassword(email, password);
-            App.setRoot("HomePage");
+            String email = textFieldEmail.getText();
+            String password = textFieldPassword.getText();
+            User user = new User(email);
+            user.setPassword(password);
+            if (user.isEmailPassEqual(admin)) {
+                App.setRoot("AdminHomePage");
+            } else if (user.isEmailPassEqual(manager)) {
+                App.setRoot("ManagerHomePage");
+            } else if (user.isEmailPassEqual(teamMember)) {
+                App.setRoot("TeamMemberHomePage");
+            } else {
+                labelLoginFailed.setVisible(true);
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
