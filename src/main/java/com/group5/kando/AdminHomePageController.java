48c97a00056e2055e8717ef0998fed674261075e
Samuel <sam@sams-mbp>
2023-04-13 17:50:21 -0400

    Modified system changes

--- src/main/java/com/group5/kando/AdminHomePageController.java
+++ src/main/java/com/group5/kando/AdminHomePageController.java
@@ -2,10 +2,33 @@ package com.group5.kando;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class AdminHomePageController {
+
+    @FXML
+    private Label ToDo1;
+    @FXML
+    private Label ToDo2;
+    @FXML
+    private Label ToDo3;
+    @FXML
+    private Label ToDo4;
+    @FXML
+    private Label ToDo5;
+    @FXML
+    private Label Label1;
+    @FXML
+    private Label OutputLabel;
 
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
-}
+
+    @FXML
+    private void OutputLabel(MouseEvent event) {
+        System.out.println("Add new components to the project.");
+        }
+    }
+    
