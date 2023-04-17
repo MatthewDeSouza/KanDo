1f17d05469d552a5da152b24280ce3971c0be97d
chrisMasoud <chrismasoud22@gmail.com>
2023-04-17 10:03:48 -0400

    Created Project class

---
+++ src/main/java/com/groupfive/kando/backend/classes/Project.java
@@ -0,0 +1,67 @@
+package com.groupfive.kando.backend.classes;
+
+import com.groupfive.kando.backend.status.Status;
+import java.util.Date;
+import java.util.Set;
+
+public class Project {
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Set<Ticket> tickets;
+    private Status status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Set<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(Set<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+}
