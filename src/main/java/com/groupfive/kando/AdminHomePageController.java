eab1b389a1c1e4531020d6be4c8f711a16a2138d
chrisMasoud <chrismasoud22@gmail.com>
2023-04-20 11:42:33 -0400

    Updated Admin controller

--- src/main/java/com/groupfive/kando/AdminHomePageController.java
+++ src/main/java/com/groupfive/kando/AdminHomePageController.java
@@ -1,11 +1,29 @@
 package com.groupfive.kando;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class AdminHomePageController {
-
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    
+    private Firestore db;
+    
+    public void initialize() {
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("key.json")))
+                    .build();
+            FirebaseApp.initializeApp(options);
+            db = FirestoreClient.getFirestore();
+        } catch (Exception e) {
+            System.out.println("Could not connect to Firestore.");
+            e.printStackTrace();
+        }
     }
+
+    
 }
