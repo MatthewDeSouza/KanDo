a576fdcf58efe3a4fdd9f1855833d85a6e21e770
chrisMasoud <chrismasoud22@gmail.com>
2023-04-30 12:42:31 -0400

    Completed TeamMemberHomePageController

--- src/main/java/com/groupfive/kando/TeamMemberHomePageController.java
+++ src/main/java/com/groupfive/kando/TeamMemberHomePageController.java
@@ -17,7 +17,6 @@ import java.util.concurrent.ExecutionException;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
