102c48c7581386c6fb0c704c98e80fa59e885873
chrisMasoud <chrismasoud22@gmail.com>
2023-04-30 10:12:49 -0400

    Updated HomePage functionality

--- src/main/java/com/groupfive/kando/AdminHomePageController.java
+++ src/main/java/com/groupfive/kando/AdminHomePageController.java
@@ -11,23 +11,38 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firestore.v1.Document;
 import com.groupfive.kando.backend.classes.Project;
+import com.groupfive.kando.backend.classes.Ticket;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 
 public class AdminHomePageController {
 
     @FXML
     private ComboBox comboBoxProject;
+    @FXML
+    private ListView listViewToDo;
+    @FXML
+    private ListView listViewDoing;
+    @FXML
+    private ListView listViewDone;
     private ObservableList<String> projects;
+    private ObservableList<Ticket> toDoTickets;
+    private ObservableList<Ticket> doingTickets;
+    private ObservableList<Ticket> doneTickets;
     private Firestore db;
 
     public void initialize() {
+        projects = comboBoxProject.getItems();
+        toDoTickets = listViewToDo.getItems();
+        doingTickets = listViewDoing.getItems();
+        doneTickets = listViewDone.getItems();
         try {
-            projects = comboBoxProject.getItems();
+            /*
             try {
                 FirebaseOptions options = new FirebaseOptions.Builder()
                         .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("key.json")))
@@ -38,13 +53,14 @@ public class AdminHomePageController {
                 System.out.println("Could not connect to Firestore.");
                 e.printStackTrace();
             }
+*/
+            db = FirestoreClient.getFirestore();
             
             ApiFuture<QuerySnapshot> query = db.collection("Projects").get();
             List<QueryDocumentSnapshot> documents = query.get().getDocuments();
             for (DocumentSnapshot document : documents) {
                 projects.add(document.get("name").toString());
             }
-            
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         } catch (ExecutionException ex) {
@@ -54,8 +70,34 @@ public class AdminHomePageController {
     }
     
     public void handleProjectSelection() {
-        String project = comboBoxProject.getValue().toString();
-        //query db for tickets and fill list views accordingly
+        try {
+            String projectName = comboBoxProject.getValue().toString();
+            ApiFuture<QuerySnapshot> query = db.collection("Tickets")
+                    .whereEqualTo("project", projectName)
+                    .get();
+            List<QueryDocumentSnapshot> documents = query.get().getDocuments();
+            for (DocumentSnapshot doc : documents) {
+                Ticket ticket = new Ticket(doc.getString("name"), doc.getString("description"));
+                switch (doc.getString("status")) {
+                    case "To Do":
+                        toDoTickets.add(ticket);
+                        break;
+                    case "Doing":
+                        doingTickets.add(ticket);
+                        break;
+                    case "Done":
+                        doneTickets.add(ticket);
+                        break;
+                    default:
+                        toDoTickets.add(ticket);
+                        break;
+                }
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } catch (ExecutionException ex) {
+            ex.printStackTrace();
+        }
     }
 }
 
