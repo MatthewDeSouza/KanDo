8ff77f7f53381080d74ea5c5d6847ba2755465f4
chrisMasoud <chrismasoud22@gmail.com>
2023-05-01 14:40:11 -0400

    Cleaned up module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.groupfive.kando {
     requires javafx.fxml;
     requires org.slf4j;
     requires ch.qos.logback.classic;
-    //requires com.google.api.apicommon;
     requires com.google.auth;
     requires com.google.auth.oauth2;
     requires firebase.admin;
@@ -14,8 +13,6 @@ module com.groupfive.kando {
 
     opens com.groupfive.kando to javafx.fxml;
     exports com.groupfive.kando;
-    exports com.groupfive.kando.backend.exception;
     exports com.groupfive.kando.backend.classes;
-    exports com.groupfive.kando.backend.status;
     opens com.groupfive.kando.backend.classes to javafx.fxml;
 }
